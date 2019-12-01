6ccc97e080ccb3f4ea7cf13a2cc44cddb9549342
Joxit <joxit972@gmail.com>
2019-12-01 10:11:16 +0100

    [osm] Add caffeine

--- osm/osm-boot/build.gradle.kts
+++ osm/osm-boot/build.gradle.kts
@@ -13,5 +13,7 @@ dependencies {
   compile(project(":osm-core"))
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-configuration-processor:")
+  implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("mil.nga.sf:sf-geojson:2.0.3")
+  implementation("com.github.ben-manes.caffeine:caffeine:2.8.0")
 }
